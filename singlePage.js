c6663f8fb91b8769c07dac40b866e706f4694702
Manoj <133344712+Manojkumar-05@users.noreply.github.com>
2023-05-29 15:21:49 +0530

    Update singlePage.js

--- singlePage.js
+++ singlePage.js
@@ -20,3 +20,13 @@ menuItem.forEach( item => {
     })
 }
 )
+
+const cursor = document.querySelector('.cursor')
+document.addEventListener('mousemove', (e) =>{
+    cursor.style.left = e.pageX + 'px'
+    cursor.style.top = e.pageY + 'px'
+})
+
+
+
+var s = skrollr.init()
